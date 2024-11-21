3f830224778ed62a0fedd37dd769b5c71e7606cc
Saumyasharma974 <sharmasaumya2464@gmail.com>
2024-11-21 21:00:14 +0530

    home page completed

--- frontend/src/Home/Hero.jsx
+++ frontend/src/Home/Hero.jsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import { useAuth } from '../context/AuthProvider';
+import React from "react";
+import { useAuth } from "../context/AuthProvider";
+import { Link } from "react-router-dom";
 
 function Hero() {
-  const { blogs, loading, error } = useAuth(); // Destructure the context values
-
-  console.log("Blogs:", blogs);
-  console.log("Loading:", loading);
-  console.log("Error:", error);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
+  const { blogs } = useAuth();
+  console.log(blogs);
   return (
-    <div>
-      <h1>Blogs</h1>
+    <div className=" container mx-auto my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-6">
       {blogs && blogs.length > 0 ? (
-        <ul>
-          {blogs.map((blog, index) => (
-            <li key={index}>{blog.title}</li> // Adjust based on your API response structure
-          ))}
-        </ul>
+        blogs.slice(0, 4).map((element) => {
+          return (
+            <Link
+              to={`/blog/${element._id}`}
+              key={element._id}
+              className="bg-white rounded-lg hover:shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300"
+            >
+              <div className="group relative">
+                <img
+                  src={element.blogImage.url}
+                  alt=""
+                  className="w-full h-56 object-cover"
+                />
+                <div className=" absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-75 group-hover:opacity-100 transition-transform duration-300"></div>
+                <h1 className=" absolute bottom-4 left-4 text-white text-xl font-bold group-hover:text-yellow-500 transition-colors duration-300">
+                  {element.title}
+                </h1>
+              </div>
+              <div className="p-6 flex items-center">
+                <img
+                  src={element.adminPhoto}
+                  alt=""
+                  className="w-12 h-12 rounded-full border-2 border-yellow-400"
+                />
+                <div className="ml-4">
+                  <p className="text-lg font-semibold text-gray-800">
+                    {element.adminName}
+                  </p>
+                  <p className="text-xs text-gray-400">New</p>
+                </div>
+              </div>
+            </Link>
+          );
+        })
       ) : (
-        <p>No blogs available</p>
+        <div className=" flex h-screen items-center justify-center">
+          Loading....
+        </div>
       )}
     </div>
   );
